7ee81e2285332265d58142d4c995096fbd6d3822
MattedPlace <61640120+MattedPlace@users.noreply.github.com>
2021-07-26 00:31:49 -0500

    Update PPP_Homework5_Solution.hs
    Updated for cohort 2

--- PPP_Homework5_Solution.hs
+++ PPP_Homework5_Solution.hs
@@ -5,17 +5,17 @@
 {-# LANGUAGE TypeApplications  #-}
 {-# LANGUAGE TypeOperators     #-}
 
-module Week04.Solution where
+module Week04.Homework where
 
-import Data.Aeson                 (FromJSON, ToJSON)
-import Data.Functor               (void)
-import Data.Text                  (Text, unpack)
-import GHC.Generics               (Generic)
+import Data.Aeson            (FromJSON, ToJSON)
+import Data.Functor          (void)
+import Data.Text             (Text, unpack)
+import GHC.Generics          (Generic)
 import Ledger
-import Ledger.Ada                 as Ada
-import Ledger.Constraints         as Constraints
-import Plutus.Contract            as Contract
-import Plutus.Trace.Emulator      as Emulator
+import Ledger.Ada            as Ada
+import Ledger.Constraints    as Constraints
+import Plutus.Contract       as Contract
+import Plutus.Trace.Emulator as Emulator
 import Wallet.Emulator.Wallet
 
 data PayParams = PayParams
@@ -23,26 +23,33 @@ data PayParams = PayParams
     , ppLovelace  :: Integer
     } deriving (Show, Generic, FromJSON, ToJSON)
 
-type PaySchema = BlockchainActions .\/ Endpoint "pay" PayParams
+type PaySchema = Endpoint "pay" PayParams
 
-payContract :: Contract () PaySchema Text ()
-payContract = do
+payContract1 :: Contract () PaySchema Text ()
+payContract1 = do
     pp <- endpoint @"pay"
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    handleError (\err -> Contract.logInfo $ "caught error: " ++ unpack err) $ void $ submitTx tx
-    payContract
+    void $ submitTx tx
 
+payContract2 :: Contract () PaySchema Text ()
+payContract2 = do 
+    Contract.handleError (\err -> Contract.logError $ "caught error: " ++ unpack err) payContract1
+    payContract2
+-- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
+-- recipient, but with amounts given by the two arguments. There should be a delay of one slot
+-- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace x y = do
-    h <- activateContractWallet (Wallet 1) payContract
-    callEndpoint @"pay" h $ PayParams
+payTrace payAmount1 payAmount2 = do
+    h1 <- activateContractWallet (Wallet 1) payContract2
+    h2 <- activateContractWallet (Wallet 2) payContract2
+    callEndpoint @"pay" h1 $ PayParams
         { ppRecipient = pubKeyHash $ walletPubKey $ Wallet 2
-        , ppLovelace  = x
+        , ppLovelace  = payAmount1
         }
     void $ Emulator.waitNSlots 1
-    callEndpoint @"pay" h $ PayParams
+    callEndpoint @"pay" h1 $ PayParams
         { ppRecipient = pubKeyHash $ walletPubKey $ Wallet 2
-        , ppLovelace  = y
+        , ppLovelace  = payAmount2
         }
     void $ Emulator.waitNSlots 1
 
